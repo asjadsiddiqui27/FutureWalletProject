b332afa6ae2e00e51803a1d274e774b012246648
asjadsiddiqui27 <asjadsiddiqui6@gmail.com>
2024-03-29 18:16:09 +0530

    Complete

--- Src/Screens/Navigation/StackNavigation.js
+++ Src/Screens/Navigation/StackNavigation.js
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,StatusBar } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native'
 import WalletName from '../Components/WalletName';
 import Onboarding from '../Components/Onboarding';
+import colors from '../../Theme/Colors';
+import fonts from '../../Theme/Fonts';
 
 
 const Stack = createNativeStackNavigator();
@@ -14,12 +16,17 @@ const Stack = createNativeStackNavigator();
 const StackNavigation = () => {
   return (
     <NavigationContainer>
+        <StatusBar backgroundColor="white" barStyle="dark-content" />
       <Stack.Navigator
         screenOptions={{
           headerStyle: {
+            // backgroundColor:"yellow"
           },
           headerShadowVisible: false,
+          headerTitleAlign: 'center',
           headerTitleStyle: {
+            color:colors.Black,
+            fontFamily:fonts.PoppinsBold
           }
         }}>
 
@@ -27,13 +34,18 @@ const StackNavigation = () => {
         <Stack.Screen
           name='onboarding'
           component={Onboarding}
-        // options={{ headerShown: false }}
+          options={{ 
+         
+            headerShown:false
+           }}
         />
 
         <Stack.Screen
           name='walletname'
           component={WalletName}
-        // options={{ headerShown: false }}
+        options={{ 
+          title:"Wallet Name"
+         }}
         />
 
 
--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-// import Onboarding from './Src/Screens/Components/Onboarding';
 import StackNavigation from './Src/Screens/Navigation/StackNavigation';
 
 AppRegistry.registerComponent(appName, () => StackNavigation);
