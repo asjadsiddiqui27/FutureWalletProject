8d739de2a8d535e6ba32d6e184cdbd821858657a
Deepdhiman052 <kaurmandeep07289@gmail.com>
2024-03-29 13:04:45 +0530

    custombtn

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Button from './Src/Screens/Common/CustomButton'
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+  <Button/>
   )
 }
 
---
+++ Src/Screens/Common/CustomButton.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+
+import colors from "../../Theme/Colors";
+
+
+
+
+function Button({
+    onPress = () => { onPressFunc() },
+    name = 'Click me',
+    buttonStyle = styles.btn,
+    textColor = styles.button,
+
+}) {
+
+    return (
+        <View style={styles.btnView}>
+            <TouchableOpacity onPress={onPress} style={buttonStyle}>
+                <Text style={textColor}>{name}</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+
+
+
+const styles = StyleSheet.create({
+
+    button: {
+        fontSize: 16,
+        fontWeight: "700",
+        textAlign: "center",
+        margin: 15,
+        color: colors.White,
+    },
+
+    btn: {
+        height: 50,
+        width: "auto",
+        marginHorizontal: 24,
+        backgroundColor: colors.background,
+        borderRadius: 12,
+
+    },
+})
+
+
+
+export default Button;
